228f51241249e6a3f85e41fd6692f2f74d428bfc
Farhan Ahmed <farhanf7n@Farhans-MacBook-Pro.local>
2022-05-25 22:57:34 +0500

    Made all files and served

--- server.js
+++ server.js
@@ -3,13 +3,33 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './pages');
+app.use(express.static(__dirname + 'asset'));
+app.use(express.static('./asset/images'));
 
-// TO DISPLAY HOMEPAGE
 app.get('/', function (request, response) {
     response.render("home");
 });
 
-// TO LOG SERVER START
+app.get('/home', function (request, response) {
+    response.render("home");
+});
+
+app.get('/new-post', function (request, response) {
+    response.render("new-post");
+});
+
+app.get('/change-settings', function (request, response) {
+    response.render("change-settings");
+});
+
+app.get('/login', function (request, response) {
+    response.render('login');
+});
+
+app.get('/register', function (request, response) {
+    response.render('register');
+});
+
 app.listen(3000, function () {
     console.log("Server started at port 3000");
 })
